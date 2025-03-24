dd257fc77a136169532cf716ad4b750df38fa90b
dhoniaridho <dhoniaridho@gmail.com>
2025-03-24 22:17:53 +0700

    feat: add ServeStaticModule for serving static files and create installation guide HTML

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,10 +5,15 @@ import { PlaybackModule } from './app/playback/playback.module';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { ENV } from './config/env';
 import { PrismaService } from './platform/prisma.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Global()
 @Module({
     imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public'),
+        }),
         PlaybackModule,
         TelegrafModule.forRoot({
             token: ENV.TELEGRAM_BOT_TOKEN,
